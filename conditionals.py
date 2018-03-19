97162e18060eafdd78d555255dc4f9a4a8e97a0d
Suhel Ahmed <suhel007@users.noreply.github.com>
2018-03-19 19:33:07 +0100

    Typo correction

--- conditionals.py
+++ conditionals.py
@@ -4,8 +4,8 @@ year = str(2017-age)
 print(name + " you born in the year "+ year)
 
 if age > 17:
-	print("You are adult! You can see a rated R movie")
+	print("You are an adult! You can see a rated R movie")
 elif age < 17 and age > 12:
-	print("You are teenager! You can see a rated PG-13 movie")
+	print("You are a teenager! You can see a rated PG-13 movie")
 else:
 	print("You are a child! You can only see rated PG movies")
